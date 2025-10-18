23d4cab978b733a1b14c3379d5bc5baa841963d0
Stiliyan1 <pinevmarketing@gmail.com>
2025-10-18 23:16:10 +0300

    Implement Auth UI & Layout

---
+++ components/forms/SelectField.tsx
@@ -0,0 +1,42 @@
+import {Label} from '@/components/ui/label'
+import React from 'react'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Controller } from 'react-hook-form';
+
+
+const SelectField = ({name, label, placeholder, options, control, error, required = false} : SelectFieldProps) => {
+  return (
+    <div className='space-y-2'>
+        <Label htmlFor={name} className='form-label'>{label}</Label>
+        <Controller 
+            name={name} 
+            control={control} 
+            rules={{ required: required ? `Please choose ${label.toLocaleLowerCase()}` : false }}
+            render={({field}) => (
+                <Select value={field.value} onValueChange={field.onChange} >
+                    <SelectTrigger className="select-trigger">
+                        <SelectValue placeholder={placeholder} />
+                    </SelectTrigger>
+                    <SelectContent className='bg-gray-800 text-white border-gray-600'>
+                        {options.map((option) => (
+                            <SelectItem value={option.value} key={option.value} className="focus:bg-gray-600 focus:text-white">
+                                {option.label}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                    {error && <p className='text-sm text-red-500'>{error.message}</p>}
+                </Select>
+            )} 
+            />
+    </div>
+  )
+}
+
+export default SelectField
